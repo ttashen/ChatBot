8ece71c6b4c3d65166b8fd3029dbf30281692fe5
bonnie shen <boyueshen@gmail.com>
2024-04-17 19:39:09 -0400

    change embedding_fn model in chain.py load_chain to match the embedding_fn model in ingest.py create_vector_stor

--- src/chain.py
+++ src/chain.py
@@ -59,7 +59,7 @@ def load_vector_store(wandb_run: wandb.run, openai_api_key: str) -> Chroma:
     vector_store_artifact_dir = wandb_run.use_artifact(
         wandb_run.config.vector_store_artifact, type="search_index"
     ).download()
-    embedding_fn = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    embedding_fn = OpenAIEmbeddings(model = "text-embedding-ada-002", openai_api_key=openai_api_key)
     # load vector store
     vector_store = Chroma(
         embedding_function=embedding_fn, persist_directory=vector_store_artifact_dir
